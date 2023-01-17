74a283c672bc6981c2b812b0650186e0dfd89405
RosehipNishikino <s17120024@alumnos.itsur.edu.mx>
2023-01-17 14:41:44 -0600

    Agregados complementos

--- DATOS_SEEMORE/Configuracion.cs
+++ DATOS_SEEMORE/Configuracion.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using MODELOS_SEEMORE;
 
 namespace DATOS_SEEMORE
 {
@@ -11,8 +13,9 @@ namespace DATOS_SEEMORE
         public static String nombre;
         public static String nombreApartado;
         public static Boolean unCambio = true;
-        public static String usuario="";
-        public static String contraseña="";
+        public static String usuario = "";
+        public static String contraseña = "";
+        public static String imagen = "";
         public static Boolean encriptacion = false;
         public static String[] Animes;
         public static int totales = 0;
@@ -31,6 +34,21 @@ namespace DATOS_SEEMORE
         public static Boolean loCerroelUsuario = false;
         public static String ruta;
         public static Boolean existeConexion = Conexion.existeConexion();
+        public class BuscarAddon {
+            public static Seemore AnimeSeleccionado { get; set; }
+            public static Cuenta cuentaSeleccionado { get; set; }
+            public static String NombreVideo { get; set; }
+            public static String RutaVideo { get; set; }
+            public static String UsuarioRegistrado { get; set; }
+            public static String UsuarioImagen { get; set; }
+            public static Boolean hayIntercambio { get; set; }
+            public static String UsuarioTemporal { get; set; }
+            public static String Usuario { get; set; }
+            public static String Imagen { get; set; }
+            public static String Sexo { get; set; }
+            public static String Contraseña { get; set; }
+            public static Boolean IsOnly { get; set; }
+        }
     }
         
     
